9b7af820aed5362d8c521034421374c976c423ea
Luiz Arthur Lima <luizarthurbolzani@gmail.com>
2024-10-01 18:03:04 -0300

    criando app recipes

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("home 1")
+
+def sobre(request):
+    return HttpResponse("sobre")
+
+def contato(request):
+    return HttpResponse("contato")
