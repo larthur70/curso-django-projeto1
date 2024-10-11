c9bb728d9de4457bb2d4acdbd1c712666c8fa42a
Luiz Arthur Lima <luizarthurbolzani@gmail.com>
2024-10-11 15:34:05 -0300

    criando a pagina de recipe detail

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html',context={
         'nome': 'Luiz Arthur Lima'
     })
+
+def recipe(request,id):
+    return render(request, 'recipes/pages/recipe-view.html',context={
+        'nome': 'Luiz Arthur Lima'
+    })
